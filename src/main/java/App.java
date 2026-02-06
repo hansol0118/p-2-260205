7794103d57f88a7d6722c0b9485d64834d554046
hansol0118 <wall197944@naver.com>
2026-02-06 12:47:47 +0900

    14 - b

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class App {
 
     private Scanner sc;
+    private WiseSayingController wiseSayingController = new WiseSayingController(sc);
+    private SystemController systemController = new SystemController();
 
     public App(Scanner sc) {
         this.sc = sc;
@@ -20,30 +22,10 @@ public class App {
             System.out.println("명령) ");
             String cmd = sc.nextLine();
             switch (cmd) {
-                case "등록" -> {
-                    System.out.print("명언 : ");
-                    String saying = sc.nextLine();
-                    System.out.print("작가 : ");
-                    String author = sc.nextLine();
-                    lastId++;
-                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
-                    wiseSayings.add(wiseSaying);
-                    System.out.print("%d번 명언이 등록되었습니다.".formatted(lastId));
-                }
-
-                case "목록" -> {
-                    System.out.println("번호 / 작가 / 명언");
-                    System.out.println("----------------------");
-
-                    wiseSayings
-                            .reversed()
-                            .stream()
-                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
-                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
-
-                }
-
+                case "등록" -> wiseSayingController.actionAdd(sc);
+                case "목록" -> wiseSayingController.actionList();
                 case "종료" -> {
+                    systemController.actionExit();
                     return;
                 }
             }
