d97f89d516b7acf13c9e0189bb820195ed524761
hansol0118 <wall197944@naver.com>
2026-02-06 12:33:38 +0900

    13 - r g

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
@@ -7,7 +9,7 @@ public class App {
     public App(Scanner sc) {
         this.sc = sc;
     }
-
+    List<WiseSaying> wiseSayings = new ArrayList<>();
     int lastId = 0;
 
     public void run() {
@@ -24,8 +26,23 @@ public class App {
                     System.out.print("작가 : ");
                     String author = sc.nextLine();
                     lastId++;
+                    WiseSaying wiseSaying = new WiseSaying(lastId, saying, author);
+                    wiseSayings.add(wiseSaying);
                     System.out.print("%d번 명언이 등록되었습니다.".formatted(lastId));
                 }
+
+                case "목록" -> {
+                    System.out.println("번호 / 작가 / 명언");
+                    System.out.println("----------------------");
+
+                    wiseSayings
+                            .reversed()
+                            .stream()
+                            .forEach(wiseSaying -> System.out.printf("%d / %s / %s%n",
+                                    wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getSaying()));
+
+                }
+
                 case "종료" -> {
                     return;
                 }
