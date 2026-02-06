397346cd53d9eb69bb64a2ae71fc3b98859c1098
hansol0118 <wall197944@naver.com>
2026-02-06 11:15:15 +0900

    6 - b

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,11 @@
+public class AppTest {
+
+        public static void run(String input){
+        //입력값
+        //명언 프로그램 실행
+
+
+        //출력값
+    }
+
+}
--- src/test/java/TestUtil.java
+++ src/test/java/TestUtil.java
@@ -1,28 +1,30 @@
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.PrintStream;
 import java.util.Scanner;
 
 public class TestUtil {
+    private static PrintStream ORIGINAL_OUT = System.out;
+    private static PrintStream CURRENT_OUT = System.out;
 
-//    public static void run(String input){
-//        //입력값
-//        Scanner scan = new Scanner(input);
-//
-//        //명언 프로그램 실행
-//
-//
-//        //출력값
-//    }
+
     public static Scanner getScanner(String input){
         return new Scanner(input);
     }
 
     public static ByteArrayOutputStream setOutByteArray() {
+
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         PrintStream printStream = new PrintStream(byteArrayOutputStream);
 
         System.setOut(printStream);
-
+        CURRENT_OUT = printStream;
         return byteArrayOutputStream;
+    }
+
+    public static void clearSetOutToByteArray(ByteArrayOutputStream outputStream) throws IOException {
+        System.setOut(ORIGINAL_OUT);
+        outputStream.close();
+        CURRENT_OUT.close();
     }
 }
