3fdf286ac519e06877c07458103e78f700764655
hansol0118 <wall197944@naver.com>
2026-02-06 10:30:24 +0900

    5 - rg

---
+++ src/test/java/TestUtilTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestUtilTest {
+    @Test
+    @DisplayName("TestUtil.genScanner()")
+    void t1() {
+
+        Scanner scanner = TestUtil.getScanner("""
+                등록
+                현재를 사랑하라.
+                작자미상
+                """
+        );
+        String cmd = scanner.nextLine();
+        String content = scanner.nextLine();
+        String author = scanner.nextLine();
+
+
+        assertThat(cmd).isEqualTo("등록");
+        assertThat(cmd).isEqualTo("현재를 사랑하라.");
+        assertThat(cmd).isEqualTo("작자미상");
+    }
+}
+//TestUtil의 필요한 것을 설계하고 작성하는게 아니라 TestUtilTest를 통해 필요한 것을 만든다
