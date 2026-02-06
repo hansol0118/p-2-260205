d5a813a481932e8f16a74a0cccbd585cd47db2d6
hansol0118 <wall197944@naver.com>
2026-02-06 10:37:44 +0900

    6 - r

--- src/test/java/TestUtilTest.java
+++ src/test/java/TestUtilTest.java
@@ -1,6 +1,7 @@
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,5 +26,23 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("현재를 사랑하라.");
         assertThat(cmd).isEqualTo("작자미상");
     }
+
+    @Test
+    @DisplayName("TestUtil.setOutByteArray()")
+    void t2() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutByteArray();
+
+        System.out.println("안녕하세요");
+
+        String rst = byteArrayOutputStream.toString();
+
+        assertThat(rst).isEqualTo("안녕하세요");
+    }
+
+
+
+
+
+
 }
 //TestUtil의 필요한 것을 설계하고 작성하는게 아니라 TestUtilTest를 통해 필요한 것을 만든다
