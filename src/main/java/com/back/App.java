609cca9970577d03f4111cc6ed88d8191da731b1
hansol0118 <wall197944@naver.com>
2026-02-06 15:13:21 +0900

    18

--- src/main/java/com/back/App.java
+++ src/main/java/com/back/App.java
@@ -1,5 +1,6 @@
 package com.back;
 
+import com.back.global.AppContext;
 import com.back.system.controller.SystemController;
 import com.back.wiseSaying.controller.WiseSayingController;
 import java.util.Scanner;
@@ -10,10 +11,10 @@ public class App {
     private WiseSayingController wiseSayingController;
     private SystemController systemController;
 
-    public App(Scanner sc) {
-        this.sc = sc;
-        this.wiseSayingController = new WiseSayingController(sc);
-        this.systemController = new SystemController();
+    public App() {
+        this.sc = AppContext.sc;
+        this.wiseSayingController = AppContext.wiseSayingController;
+        this.systemController =  AppContext.systemController;
     }
 
     public void run() {
